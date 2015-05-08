ec070ade32832f7d7206413c2cc88659a7c05303
Erik Marty <emarty@broadway.com>
2015-05-08 06:12:51 -0400

    Minifying css

--- gulp/tasks/dev/css.js
+++ gulp/tasks/dev/css.js
@@ -1,8 +1,24 @@
 var gulp = require('gulp');
 var minifyCss = require('gulp-minify-css');
 
+var concat = require('gulp-concat');
+
+var uglify = require('gulp-uglify');
+
+//var rename = require('gulp-rename');
+//
+//gulp.task('uglify-css', function() {
+//    return gulp.src('../../../static/assets/css/*.css')
+//        .pipe(uglify())
+//        .pipe(rename({suffix: '.min'}))
+//        .pipe(gulp.dest('../../../static/assets/css/'));
+//});
+
+
 gulp.task('minify-css', function() {
-    return gulp.src('../../../static/assets/css/*.css')
-        .pipe(minifyCss())
-        .pipe(gulp.dest('dist'));
+    gulp.src('../../../static/assets/css/*.css')
+        //.pipe(minifyCss())
+        .pipe(concat('style.min.css'))
+        .pipe(uglify())
+        .pipe(gulp.dest('../../../static/assets/css/'))
 });
